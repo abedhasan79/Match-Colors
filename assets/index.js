52e973adedf077ed7d5e381feccc877837e64500
Abed Hasan <abedhasan79@gmail.com>
2022-11-11 00:40:55 -0500

    added service worker and manfest.json

--- assets/index.js
+++ assets/index.js
@@ -141,3 +141,25 @@ document.getElementById('restart').addEventListener('click', ()=>{
     location.reload();
 });
 
+
+
+const installBtn = document.getElementById('installBtn');
+const textHeader = document.getElementById('textHeader');
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  installBtn.style.visibility = 'visible';
+  textHeader.textContent = 'Click the button to install!';
+
+  installBtn.addEventListener('click', () => {
+    event.prompt();
+    installBtn.setAttribute('disabled', true);
+    installBtn.textContent = 'Installed!';
+  });
+});
+
+window.addEventListener('appinstalled', (event) => {
+    installBtn.setAttribute('style', 'display:none');
+  textHeader.textContent = 'Successfully installed!';
+  console.log('👍', 'appinstalled', event);
+});
